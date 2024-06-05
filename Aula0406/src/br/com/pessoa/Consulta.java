8d98b24d69440adae50431cb3d2efcadc9fabc29
julianojfo <160608534+julianojfo@users.noreply.github.com>
2024-06-04 21:38:02 -0300

    Adicionando Inicio do Projeto de Clínica

---
+++ Aula0406/src/br/com/pessoa/Consulta.java
@@ -0,0 +1,16 @@
+package br.com.pessoa;
+
+public class Consulta {
+	private String dataConsulta;
+
+	public String getDataConsulta() {
+		return dataConsulta;
+	}
+
+	public void setDataConsulta(String dataConsulta) {
+		this.dataConsulta = dataConsulta;
+	}
+	
+	
+
+}
